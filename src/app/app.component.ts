5ac11bda6384a0be336de0596656a2bfcafe97ce
SIDDHARTH GORE <sidgore@sid.local>
2017-08-16 20:26:21 -0700

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hello</h1>
+  <br>
+  <p>{{myObject.location}}</p>
+  
+  <ul>
+<li *ngFor="let r of myArr">{{r}}</li>  
+  </ul>
+
+  <button [disabled] ="buttonStatus" name="My Button"></button>
+
+
+ 
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works';
+
+
+  myObject={
+    gender:'male',
+    age:33,
+    location:'San Jose',
+
+  };
+buttonStatus = false;
+  myArr=['him','hers','yours'];
+
+
+
+   angularLogo = 'https://angular.io/assets/images/support/angular-404.svg';
 }
